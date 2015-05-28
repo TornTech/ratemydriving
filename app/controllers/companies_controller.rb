309a405c12d97ab291eda6fdeec8120ca3ba6417
David Wen <dstwen@gmail.com>
2015-05-28 16:04:48 -0700

    Server side creation of company object

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,36 @@
 class CompaniesController < ApplicationController
   def index
+    @text = "Company list:"
+    @companies = Company.all
+  end
+
+  def new
+    @company = Company.new 
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      # flash[:notice] = "#{@company.title} has been successfully submitted."
+      redirect_to companies_path
+
+    else
+      # flash.now[:alert] = "Please make sure all fields are filled in correctly :)"
+      render "new"
+    end
+  end
+
+  private 
+
+  def company_params
+    params.require(:company).permit(
+      :name,
+      :email,
+      :phone,
+      :contact_name,
+      :created_at,
+      :updated_at
+    )
   end
 end
