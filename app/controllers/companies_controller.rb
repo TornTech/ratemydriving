a71838f71efa24a142978c53120ea846cdf7f8d6
David Wen <dstwen@gmail.com>
2015-05-28 16:14:42 -0700

    Edit and update method for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -21,6 +21,24 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.assign_attributes(company_params)
+
+    if @company.save
+      # flash[:notice] = "#{@company.title} has been successfully submitted."
+      redirect_to companies_path
+
+    else
+      # flash.now[:alert] = "Please make sure all fields are filled in correctly :)"
+      render "edit"
+    end
+  end
+
   private 
 
   def company_params
