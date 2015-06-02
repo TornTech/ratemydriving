112428fc70f2c01fed6be3f5d6f1ad259396688e
David Wen <dstwen@gmail.com>
2015-06-02 16:28:56 -0700

    Made contact page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :cars
 
   get "about" => "pages#about", as: :about
+  get "contact" => "pages#contact", as: :contact
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
