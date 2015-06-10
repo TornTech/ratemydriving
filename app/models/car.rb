47a3c0cf1f7be99e4bd7d6f3f29c824876fa7612
David Wen <dstwen@gmail.com>
2015-06-10 16:52:57 -0700

    Made license plates unique

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,5 @@
 class Car < ActiveRecord::Base
   belongs_to :company
   validates :code, uniqueness: { message: "code has already been taken" }
+  validates :license_plate, uniqueness: { message: "license plate has already been taken" }
 end
