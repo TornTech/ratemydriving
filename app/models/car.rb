e310e77bff0f9ad0b543472cedd9b915537150f7
David Wen <dstwen@gmail.com>
2015-06-19 16:08:26 -0700

    Created models page

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ActiveRecord::Base
   belongs_to :company
+  has_many :submissions
   validates :code, uniqueness: { message: "code has already been taken" }
   validates :license_plate, uniqueness: { message: "license plate has already been taken" }
 end
