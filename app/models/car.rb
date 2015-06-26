45c8f835d2fa024022f428e06eb9356102aca792
David Wen <dstwen@gmail.com>
2015-06-26 16:54:05 -0700

    Code and license plates are automatically saved as upper case when saved into database

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,14 @@ class Car < ActiveRecord::Base
   has_many :submissions
   validates :code, uniqueness: { message: "code has already been taken" }
   validates :license_plate, uniqueness: { message: "license plate has already been taken" }
+
+  before_save :upcase_code, :upcase_license_plate
+
+  def upcase_code
+    self.code = self.code.upcase
+  end
+
+  def upcase_license_plate
+    self.license_plate = self.license_plate.upcase
+  end
 end
