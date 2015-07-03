157f4cbb0e06655369d3353c6fc0a2f3db2d1373
David Wen <dstwen@gmail.com>
2015-07-03 16:19:30 -0700

    Validates empty boxes when creating new company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ActiveRecord::Base
   has_many :cars
+  validates :name, presence: true
+  validates :contact_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
 end
