b9f244d14e41b0a11e420ba2a2ded0022cdffa38
David Wen <dstwen@gmail.com>
2015-07-03 16:48:04 -0700

    Car creation button auto selects company from admin page

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -9,4 +9,10 @@ function admin() {
     $(".companies-list-admin-selected").removeClass("companies-list-admin-selected")
     $(this).addClass("companies-list-admin-selected")
   })
+
+  $("body").on("click", ".button-car", function(e) {
+    e.preventDefault()
+    var selectedCompany = $(".companies-list-admin-selected").attr("data-company-id")
+    window.location = "/cars/new?id=" + selectedCompany
+  })
 }
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "about" => "pages#about", as: :about
   get "contact" => "pages#contact", as: :contact
   get "submit/:code" => "submissions#submit", as: :submit
+  # get "cars/:code" => "admins#submit", as: :submit
   post "submission" => "submissions#create", as: :create_submission
 
 
